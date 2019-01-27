366db8789b07f805e0738f812a986edd71b38f69
Daniel Simko <daniel@simko.xyz>
2019-01-27 19:16:54 +0100

    nrf24l01 driver: transmitting and receiving data, some features can be configured
    note: using only one nrf24l01(+) device is supported; this driver was
    not tested (built referencing only to the nrf24l01 datasheet)
    
    missing features:
    	* more fine-grained control
    	* support for multiple pipes (only pipe 0 is supported at the moment)
    	* choosing crc mode (1/2 bits)
    	* many other

--- src/drivers/wireless/nrf24l01.c
+++ src/drivers/wireless/nrf24l01.c
@@ -24,44 +24,61 @@
  * Registers
  */
 #define NRF24L01_REG_CONFIG 0x00
-#define NRF24L01_REG_ENAA 0x01
-#define NRF24L01_REG_ENRXADDR 0x02
-#define NRF24L01_REG_SETUPAW 0x03
-#define NRF24L01_REG_SETUPRETR 0x04
-#define NRF24L01_REG_RFCH 0x05
-#define NRF24L01_REG_RFSETUP 0x06
+#define NRF24L01_REG_EN_AA 0x01
+#define NRF24L01_REG_EN_RXADDR 0x02
+#define NRF24L01_REG_SETUP_AW 0x03
+#define NRF24L01_REG_SETUP_RETR 0x04
+#define NRF24L01_REG_RF_CH 0x05
+#define NRF24L01_REG_RF_SETUP 0x06
 #define NRF24L01_REG_STATUS 0x07
-#define NRF24L01_REG_OBSERVETX 0x08
-#define NRF24L01_REG_CD 0x09
-#define NRF24L01_REG_RXADDRP0 0x0a
-#define NRF24L01_REG_RXADDRP1 0x0b
-#define NRF24L01_REG_RXADDRP2 0x0c
-#define NRF24L01_REG_RXADDRP3 0x0d
-#define NRF24L01_REG_RXADDRP4 0x0e
-#define NRF24L01_REG_RXADDRP5 0x0f
-#define NRF24L01_REG_TXADDR 0x10
-#define NRF24L01_REG_RXPWP0 0x11
-#define NRF24L01_REG_RXPWP1 0x12
-#define NRF24L01_REG_RXPWP2 0x13
-#define NRF24L01_REG_RXPWP3 0x14
-#define NRF24L01_REG_RXPWP4 0x15
-#define NRF24L01_REG_RXPWP5 0x16
-#define NRF24L01_REG_FIFOSTATUS 0x17
+#define NRF24L01_REG_OBSERVE_TX 0x08
+#define NRF24L01_REG_RPD 0x09
+#define NRF24L01_REG_RX_ADDR_P0 0x0a
+#define NRF24L01_REG_RX_ADDR_P1 0x0b
+#define NRF24L01_REG_RX_ADDR_P2 0x0c
+#define NRF24L01_REG_RX_ADDR_P3 0x0d
+#define NRF24L01_REG_RX_ADDR_P4 0x0e
+#define NRF24L01_REG_RX_ADDR_P5 0x0f
+#define NRF24L01_REG_TX_ADDR 0x10
+#define NRF24L01_REG_RX_PW_P0 0x11
+#define NRF24L01_REG_RX_PW_P1 0x12
+#define NRF24L01_REG_RX_PW_P2 0x13
+#define NRF24L01_REG_RX_PW_P3 0x14
+#define NRF24L01_REG_RX_PW_P4 0x15
+#define NRF24L01_REG_RX_PW_P5 0x16
+#define NRF24L01_REG_FIFO_STATUS 0x17
 #define NRF24L01_REG_DYNDP 0x1c
 #define NRF24L01_REG_FEATURE 0x1d
+
+/*
+ * Config register
+ */
+#define NRF24L01_REG_CONFIG_MASK_RX_DR 0x06
+#define NRF24L01_REG_CONFIG_MASK_TX_DS 0x05
+#define NRF24L01_REG_CONFIG_MASK_MAX_RT 0x04
+#define NRF24L01_REG_CONFIG_EN_CRC 0x03
+#define NRF24L01_REG_CONFIG_CRCO 0x02
+#define NRF24L01_REG_CONFIG_PWR_UP 0x01
+#define NRF24L01_REG_CONFIG_PRIM_RX 0x00
+
+/*
+ * Status register
+ */
+#define NRF24L01_REG_STATUS_RX_DR 0x06
+#define NRF24L01_REG_STATUS_TX_DS 0x05
+#define NRF24L01_REG_STATUS_MAX_RT 0x04
+#define NRF24L01_REG_STATUS_RX_P_NO 0x01
+#define NRF24L01_REG_STATUS_TX_FULL 0x00
 
 typedef struct {
 	uint8_t size;
 	uint8_t reg_size[];
 } nrf24l01_regs;
 
-/*
- * Register buffer
- */
-typedef struct {
-	uint8_t size;
-	uint8_t data[NRF24L01_REG_BUF_MAX];
-} nrf24l01_reg_buf;
+uint32_t spi;
+void (*rxdr_callback)(nrf24l01_payload payload) = 0;
+void (*txds_callback)(void) = 0;
+void (*maxrt_callback)(void) = 0;
 
 /*
  * Registers
@@ -125,39 +142,200 @@ const nrf24l01_regs nrf24l01_reg_def = {
 	}
 };
 
-void nrf24l01_read_reg(uint8_t reg, nrf24l01_reg_buf *buf);
+void nrf24l01_read_reg(uint8_t reg, uint8_t *buf);
 
-void nrf24l01_write_reg(uint8_t reg, nrf24l01_reg_buf *buf);
+void nrf24l01_write_reg(uint8_t reg, uint8_t *buf);
 
-void nrf24l01_init(uint32_t spi) {
+void nrf24l01_config(uint8_t field, uint8_t value);
+
+uint8_t nrf24l01_read_status(uint8_t field);
+
+void nrf24l01_clear_status(uint8_t field);
+
+void nrf24l01_init(uint32_t spi_dev) {
 	spi_init();
 
+	spi = spi_dev;
+
+	/*
 	spi_select();
-	spi_drv_read(SPI1);
+	spi_drv_read(spi);
 
 	spi_deselect();
+	*/
 }
 
-void nrf24l01_read_reg(uint8_t reg, nrf24l01_reg_buf *buf) {
+/*
+ * Reading/writing registers
+ */
+void nrf24l01_read_reg(uint8_t reg, uint8_t *buf) {
 	spi_select();
 
-	spi_drv_xfer(SPI1, (NRF24L01_CMD_R_REGISTER | reg));
+	spi_drv_xfer(spi, (NRF24L01_CMD_R_REGISTER | reg));
 
 	for (uint8_t i = 0; i < nrf24l01_reg_def.reg_size[reg]; i++) {
-		buf->data[i] = spi_drv_xfer(SPI1, NRF24L01_CMD_NOP);
+		buf[i] = spi_drv_xfer(spi, NRF24L01_CMD_NOP);
 	}
 
 	spi_deselect();
 }
 
-void nrf24l01_write_reg(uint8_t reg, nrf24l01_reg_buf *buf) {
+void nrf24l01_write_reg(uint8_t reg, uint8_t *buf) {
 	spi_select();
 
-	spi_drv_xfer(SPI1, (NRF24L01_CMD_W_REGISTER | reg));
+	spi_drv_xfer(spi, (NRF24L01_CMD_W_REGISTER | reg));
 
 	for (uint8_t i = 0; i < nrf24l01_reg_def.reg_size[reg]; i++) {
-		spi_drv_xfer(SPI1, buf->data[i]);
+		spi_drv_xfer(spi, buf[i]);
 	}
 
 	spi_deselect();
+
+}
+
+/*
+ * Configuration
+ */
+void nrf24l01_config(uint8_t field, uint8_t value) {
+	uint8_t config_reg;
+
+	nrf24l01_read_reg(NRF24L01_REG_CONFIG, &config_reg);
+
+	config_reg &= ~(0x01 << field);
+	config_reg |= (value << field);
+
+	nrf24l01_write_reg(NRF24L01_REG_CONFIG, &config_reg);
+}
+
+void nrf24l01_enable_crc(void) {
+	nrf24l01_config(NRF24L01_REG_CONFIG_EN_CRC, 1);
+}
+
+void nrf24l01_disable_crc(void) {
+	nrf24l01_config(NRF24L01_REG_CONFIG_EN_CRC, 0);
+}
+
+void nrf24l01_enable_power(void) {
+	nrf24l01_config(NRF24L01_REG_CONFIG_PWR_UP, 1);
+}
+
+void nrf24l01_disable_power(void) {
+	nrf24l01_config(NRF24L01_REG_CONFIG_PWR_UP, 0);
+}
+
+void nrf24l01_mode(uint8_t mode) {
+	nrf24l01_config(NRF24L01_REG_CONFIG_PRIM_RX, mode);
+}
+
+/*
+ * Read status
+ */
+uint8_t nrf24l01_read_status(uint8_t field) {
+	uint8_t status;
+
+	nrf24l01_read_reg(NRF24L01_REG_STATUS, &status);
+
+	/* return only the value of the requested field */
+	return ((status & (1 << field)) >> field);
+}
+
+void nrf24l01_clear_status(uint8_t field) {
+	uint8_t reg = (1 << field);
+
+	nrf24l01_write_reg(NRF24L01_REG_STATUS, &reg);
+}
+
+/*
+ * Send/Receive data
+ */
+int nrf24l01_send(nrf24l01_payload *payload) {
+	if (nrf24l01_read_status(NRF24L01_REG_STATUS_TX_FULL) == 1) {
+		spi_select();
+		spi_drv_xfer(spi, NRF24L01_CMD_FLUSH_TX);
+		spi_deselect();
+	}
+
+	spi_select();
+
+	spi_drv_xfer(spi, NRF24L01_CMD_W_TX_PAYLOAD);
+
+	for (uint8_t i = 0; i < payload->size; i++) {
+		spi_drv_xfer(spi, payload->data[i]);
+	}
+
+	spi_deselect();
+
+	return 0;
+}
+
+int nrf24l01_receive(nrf24l01_payload *payload) {
+	/* Read number of received bytes in pipe 0 */
+	uint8_t rx_pw;
+
+	nrf24l01_read_reg(NRF24L01_REG_RX_PW_P0, &rx_pw);
+
+	/* read received bytes */
+	spi_select();
+
+	spi_drv_xfer(spi, NRF24L01_CMD_R_RX_PAYLOAD);
+
+	for (uint8_t i = 0; i < rx_pw; i++) {
+		spi_drv_xfer(spi, payload->data[i]);
+	}
+
+	payload->size = rx_pw;
+
+	spi_deselect();
+
+	return 0;
+}
+
+/*
+ * Setup callback functions
+ */
+void nrf24l01_rxdr_callback(void (*callback_func)(nrf24l01_payload payload)) {
+	rxdr_callback = callback_func;
+}
+
+void nrf24l01_txds_callback(void (*callback_func)(void)) {
+	txds_callback = callback_func;
+}
+
+void nrf24l01_maxrt_callback(void (*callback_func)(void)) {
+	maxrt_callback = callback_func;
+}
+
+/*
+ * IRQ handler
+ */
+void nrf24l01_irq(void) {
+	uint8_t status;
+	nrf24l01_read_reg(NRF24L01_REG_STATUS, &status);
+
+	if ((status & (1 << NRF24L01_REG_STATUS_RX_DR)) != 0) {
+		/* Received data */
+		if (rxdr_callback != 0) {
+			nrf24l01_payload rxdata;
+			nrf24l01_receive(&rxdata);
+			rxdr_callback(rxdata);
+		} else {
+			spi_select();
+			spi_drv_xfer(spi, NRF24L01_CMD_FLUSH_RX);
+			spi_deselect();
+		}
+
+		nrf24l01_clear_status(NRF24L01_REG_STATUS_RX_DR);
+	} else if ((status & (1 << NRF24L01_REG_STATUS_TX_DS)) != 0) {
+		/* Successful transmission (received ACK) */
+		if (txds_callback != 0)
+			txds_callback();
+
+		nrf24l01_clear_status(NRF24L01_REG_STATUS_TX_DS);
+	} else if ((status & (1 << NRF24L01_REG_STATUS_MAX_RT)) != 0) {
+		/* Maximum retransmit count reached (probably an unsucessful transmission) */
+		if (maxrt_callback != 0)
+			maxrt_callback();
+
+		nrf24l01_clear_status(NRF24L01_REG_STATUS_MAX_RT);
+	}
 }
--- src/drivers/wireless/nrf24l01.h
+++ src/drivers/wireless/nrf24l01.h
@@ -1,6 +1,9 @@
 #ifndef __NRF24L01_H__
 #define __NRF24L01_H__
 
+#define NRF24L01_MODE_TX 0x00
+#define NRF24L01_MODE_RX 0x01
+
 #define NRF24L01_PAYLOAD_MAX 32
 
 typedef struct {
@@ -8,8 +11,56 @@ typedef struct {
 	uint8_t data[NRF24L01_PAYLOAD_MAX];
 } nrf24l01_payload;
 
+/*
+ * Initialize SPI bus and configure the device
+ */
 void nrf24l01_init(uint32_t spi);
 
+/*
+ * Enable CRC
+ */
+void nrf24l01_enable_crc(void);
+
+/*
+ * Disable CRC
+ */
+void nrf24l01_disable_crc(void);
+
+/*
+ * Power up
+ */
+void nrf24l01_enable_power(void);
+
+/*
+ * Power down
+ */
+void nrf24l01_disable_power(void);
+
+/*
+ * Set mode (RX/TX)
+ * (ref.: NRF24L01_MODE_TX, NRF24L01_MODE_RX)
+ */
+void nrf24l01_mode(uint8_t mode);
+
+/*
+ * Send/receive data
+ */
 int nrf24l01_send(nrf24l01_payload *payload);
+int nrf24l01_receive(nrf24l01_payload *payload);
+
+/*
+ * setup callback functions for the irq handler
+ */
+void nrf24l01_rxdr_callback(void (*callback_func)(nrf24l01_payload payload));
+void nrf24l01_txds_callback(void (*callback_func)(void));
+void nrf24l01_maxrt_callback(void (*callback_func)(void));
+
+/*
+ * IRQ handler
+ * EXTI must be configured and call this function
+ *	can be setup to trigger an action on receive/transmit/failed transmit events
+ *	(see callback funcs)
+ */
+void nrf24l01_irq(void);
 
 #endif /* __NRF24L01_H__ */
