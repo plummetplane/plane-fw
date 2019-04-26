3f4b61f2dbad39f9529bb38e14417d3482a50d4e
Daniel Simko <daniel@simko.xyz>
2019-04-26 14:59:28 +0200

    inial work on plane fw: receiving commands, setting servo duty, calculating average pressure

--- src/command/cmds.h
+++ src/command/cmds.h
@@ -3,4 +3,6 @@
 
 #define CMD_SERVO_CONTROL 0x02
 
+#define CMD_REQ_TELEMETRY 0x03
+
 #endif /* __CMDS_H__ */
---
+++ src/plane.c
@@ -0,0 +1,120 @@
+#include <libopencm3/cm3/nvic.h>
+#include <libopencm3/stm32/exti.h>
+#include <libopencm3/stm32/gpio.h>
+#include <libopencm3/stm32/rcc.h>
+
+#include "drivers/misc/bmp280-i2c.h"
+#include "drivers/pwm/pwm.h"
+#include "drivers/wireless/nrf24l01.h"
+
+#include "command/cmds.h"
+
+/*
+ * TODO: timer to query barometric pressure and calculate avg, telemetry request handler
+ */
+
+#define BMP_I2C 0
+#define BMP_SAMPLE_COUNT 32
+
+uint32_t pres_hist[32];
+uint8_t pres_pos = 0;
+uint64_t pres_sum = 0;
+
+void nrf_recv_callback(nrf24l01_payload payload);
+
+static void clock_setup(void) {
+	rcc_clock_setup_in_hse_8mhz_out_72mhz();
+}
+
+static void tim_setup(void) {
+	rcc_periph_clock_enable(RCC_TIM3);
+	nvic_enable_irq(NVIC_TIM3_IRQ);
+	rcc_periph_reset_pulse(RST_TIM3);
+	timer_set_mode(TIM3, TIM_CR1_CKD_CK_INT, TIM_CR1_CMS_EDGE, TIM_CR1_DIR_UP);
+	timer_set_prescaler(TIM3, (rcc_apb1_frequency * 2));
+	timer_disable_preload(TIM3);
+	timer_continuous_mode(TIM3);
+	timer_set_period(TIM3, 0xff);
+	timer_set_oc_value(TIM3, TIM_OC1, 65535);
+	timer_enable_counter(TIM3);
+	timer_enable_irq(TIM3, TIM_DIER_CC1IE);
+}
+
+static void pwm_setup(void) {
+	pwm_tim_setup(TIM4, 72, 20000);
+	pwm_pin_setup(TIM4, TIM_OC4);
+	pwm_pin_setup(TIM4, TIM_OC3);
+	pwm_set_duty(TIM4, TIM_OC4, 1500);
+	pwm_set_duty(TIM4, TIM_OC3, 1500);
+}
+
+static void nrf24l01_setup(void) {
+	nrf24l01_init(0, GPIOB, GPIO0, NRF24L01_MODE_RX);
+	nrf24l01_rxdr_callback(&nrf_recv_callback);
+
+	rcc_periph_clock_enable(RCC_GPIOB);
+	rcc_periph_clock_enable(RCC_AFIO);
+
+	nvic_enable_irq(NVIC_EXTI0_IRQ);
+
+	gpio_set_mode(GPIOB, GPIO_MODE_INPUT, GPIO_CNF_INPUT_FLOAT, GPIO1);
+
+	exti_select_source(EXTI0, GPIOB);
+	exti_set_trigger(EXTI0, EXTI_TRIGGER_FALLING);
+	exti_enable_request(EXTI0);
+}
+
+static void bmp_setup(void) {
+	bmp280_i2c_init(BMP_I2C);
+
+	for (uint32_t i = 0; i < BMP_SAMPLE_COUNT; i++) {
+		pres_hist[i] = bmp280_i2c_pres(0);
+		pres_sum += pres_hist[i];
+	}
+}
+
+void servo_pwm_duty(uint8_t *ppm) {
+	pwm_set_duty(TIM4, TIM_OC4, ppm[0]);
+	pwm_set_duty(TIM4, TIM_OC3, ppm[1]);
+}
+
+void nrf_recv_callback(nrf24l01_payload payload) {
+	switch (payload.data[0]) {
+		case CMD_SERVO_CONTROL:
+			servo_pwm_duty(&payload.data[1]);
+			break;
+		case CMD_REQ_TELEMETRY:
+			/* handle telemetry */
+			break;
+	}
+}
+
+void tim3_isr(void) {
+	if (timer_get_flag(TIM3, TIM_SR_CC1IF)) {
+		timer_clear_flag(TIM3, TIM_SR_CC1IF);
+
+		pres_sum -= pres_hist[pres_pos];
+		pres_hist[pres_pos] = bmp280_i2c_pres(BMP_I2C);
+		pres_sum += pres_hist[pres_pos];
+
+		pres_pos++;
+
+		if (pres_pos >= BMP_SAMPLE_COUNT)
+			pres_pos = 0;
+	}
+}
+
+void exti0_isr(void) {
+	nrf24l01_irq();
+	exti_reset_request(EXTI0);
+}
+
+int main(void) {
+	clock_setup();
+	nrf24l01_setup();
+
+	while(1)
+		__asm("wfi");
+
+	return 0;
+}
